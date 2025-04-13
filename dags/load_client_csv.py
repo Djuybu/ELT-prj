6498de7db904e7f67e033c5aeb4db4606246da20
Djuybu <macminhduy2004@gmail.com>
2025-04-13 10:16:03 +0700

    Config Hadoop

--- dags/load_client_csv.py
+++ dags/load_client_csv.py
@@ -9,8 +9,11 @@ spark = SparkSession.builder \
     .appName("DeltaLakeExample") \
     .master("local[*]") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config('spark.jars', '/opt/spark/jar/delta-spark_2.12-3.3.1.jar') \
+    .config('spark.jars', '/opt/spark/jars/delta-spark_2.12-3.3.1.jar') \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.hadoop.fs.defaultFS", "hdfs://10.128.0.2:9000") \
+    .config("spark.hadoop.hadoop.home.dir", "/opt/hadoop")  \
+    .config("spark.hadoop.yarn.resourcemanager.address", "hdfs://10.128.0.2:8032")\
     .getOrCreate()
 
 # create dataframe from csv file
@@ -34,8 +37,8 @@ def check_data(df: DataFrame) -> bool:
         return False
     
 def load_to_delta(df: DataFrame) -> None:
-    # save delta table to localhost hdfs
-    df.write.format("delta").mode("overwrite").save("hdfs://host.docker.internal:9000/delta/client")
+    # save delta table to Hadoop HDFS
+    df.write.format("delta").mode("overwrite").save("hdfs://10.128.0.2:9000/delta/client")  # Thay đổi IP cho phù hợp
 
 if __name__ == "__main__":
     # Example usage
