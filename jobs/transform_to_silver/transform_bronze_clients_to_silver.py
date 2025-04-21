10b4c5a39e075ca6cbd4f62a23fe5f445d72ebed
Djuybu <macminhduy2004@gmail.com>
2025-04-21 16:48:01 +0700

    Fix micronanobug

--- jobs/transform_to_silver/transform_bronze_clients_to_silver.py
+++ jobs/transform_to_silver/transform_bronze_clients_to_silver.py
@@ -3,9 +3,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, current_timestamp, row_number
 from pyspark.sql.window import Window
 from delta.tables import DeltaTable
-from py4j.java_gateway import java_import
 
-# Khởi tạo SparkSession
+# ============================
+# ⚙️ Spark Session Config
+# ============================
 sc = SparkContext.getOrCreate()
 spark = SparkSession.builder \
     .appName("MergeBronzeToSilver") \
@@ -14,36 +15,27 @@ spark = SparkSession.builder \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
-    .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
     .config("spark.hadoop.google.cloud.auth.service.account.enable", "false") \
     .getOrCreate()
 
-# Đường dẫn input/output
+
 bronze_path = "gs://bigdata-team3-uet-zz/bronze/clients"
 silver_path = "gs://bigdata-team3-uet-zz/silver/clients"
 primary_key = "client_id"
 
-# Đọc dữ liệu từ bronze (trong 7 ngày gần nhất)
+
 bronze_df = spark.read.format("delta").load(bronze_path) \
     .filter("ingestion_time >= current_timestamp() - INTERVAL 7 DAYS")
 
-# Lấy bản ghi mới nhất cho mỗi client
+
 window_spec = Window.partitionBy(primary_key).orderBy(col("ingestion_time").desc())
 latest_df = bronze_df.withColumn("row_num", row_number().over(window_spec)) \
     .filter("row_num = 1") \
     .drop("row_num", "source_file", "ingestion_time")
 
-# Import Java URI và Path
-java_import(spark._jvm, "java.net.URI")
-java_import(spark._jvm, "org.apache.hadoop.fs.Path")
-
-# Tạo FileSystem với URI đúng
-uri = spark._jvm.URI(silver_path)
-fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(uri, spark._jsc.hadoopConfiguration())
-
-# Kiểm tra xem silver_path đã tồn tại chưa
-if fs.exists(spark._jvm.Path(silver_path)):
+if DeltaTable.isDeltaTable(spark, silver_path):
     silver_table = DeltaTable.forPath(spark, silver_path)
+
     silver_table.alias("target").merge(
         latest_df.alias("source"),
         f"target.{primary_key} = source.{primary_key}"
