8d76df62c97e9731ef058328c3fc7c3dabad43ea
Djuybu <macminhduy2004@gmail.com>
2025-04-21 23:46:30 +0700

    Add data

---
+++ dags/update_data/update_store_data.py
@@ -0,0 +1,50 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 4, 20),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='clients_etl_pipeline',
+    default_args=default_args,
+    schedule_interval='@weekly',
+    catchup=False,
+    description='ETL pipeline from CSV to bronze to silver layer',
+    tags=['delta', 'bronze', 'silver']
+) as dag:
+
+    # 1️⃣ Load raw CSV lên Bronze
+    load_to_bronze = BashOperator(
+        task_id='load_csv_to_bronze',
+        bash_command="""
+        /opt/spark/bin/spark-submit \
+        --master local[*] \
+        --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
+        --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog \
+        --conf spark.hadoop.fs.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem \
+        --jars /opt/spark/jars/delta-spark_2.13-3.3.0.jar,/opt/spark/jars/delta-storage-3.3.0.jar,/opt/spark/jars/gcs-connector-hadoop3-latest.jar \
+        /opt/airflow/jobs/upload_to_bronze/update_raw_csv_stores_to_bronze.py
+        """
+    )
+
+    # 2️⃣ Transform từ Bronze sang Silver
+    transform_to_silver = BashOperator(
+        task_id='transform_bronze_to_silver',
+        bash_command="""
+        pip install delta-spark==2.4.0 && \
+        /opt/spark/bin/spark-submit \
+        --master local[*] \
+        --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
+        --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog \
+        --conf spark.hadoop.fs.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem \
+        --jars /opt/spark/jars/delta-spark_2.13-3.3.0.jar,/opt/spark/jars/delta-storage-3.3.0.jar,/opt/spark/jars/gcs-connector-hadoop3-latest.jar \
+        /opt/airflow/jobs/transform_to_silver/transform_bronze_stores_to_silver.py
+        """
+    )
+
+    load_to_bronze >> transform_to_silver
---
+++ jobs/upload_to_bronze/update_raw_csv_products_to_bronze.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import os
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import input_file_name, current_timestamp
+
+folder_path = "/opt/airflow/files/raw/products"
+
+sc = SparkContext.getOrCreate()
+spark = SparkSession.builder \
+    .appName("DeltaLakeToGCS") \
+    .master("local[*]") \
+    .config("spark.jars", "/opt/spark/jars/delta-spark_2.12-3.3.1.jar,/opt/spark/jars/gcs-connector-hadoop3-2.2.4-shaded.jar") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+    .config("spark.hadoop.google.cloud.auth.service.account.enable", "false") \
+    .getOrCreate()
+
+def process_csv_file(file_path: str):
+    print(f"Processing file: {file_path}")
+    df = spark.read.option("header", True).csv(file_path)
+
+    # Gắn thông tin metadata
+    df = df.withColumn("ingestion_time", current_timestamp()) \
+           .withColumn("source_file", input_file_name())
+
+    # Append vào delta table
+    df.write.format("delta") \
+        .mode("append") \
+        .save("gs://bigdata-team3-uet-zz/bronze/products")
+
+if __name__ == "__main__":
+    # Example usage
+        # Lấy danh sách các file CSV
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".csv"):
+            file_path = os.path.join(folder_path, file_name)
+            process_csv_file(file_path)
+
+    print("✅ All files processed.")
